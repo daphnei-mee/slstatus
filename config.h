aadbaaa141450e6f76334c0e00fb67e0c940d2b9
Daphnei Mee <jacobmee@tutanota.com>
2020-08-25 20:55:25 -0400

    changed pulseaudio-ctl to pamixer

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ run_command, "[VOL:%4s%%] ", "pamixer --get-volume" },
+	{ run_command, "[VOL:%4s] ", "pulseaudio-ctl current" },
 	{ battery_perc, "[BAT:%s%%] ", "BAT0"            },
 	{ cpu_perc, "[CPU:%s%%] ", NULL	      },
 	{ ram_perc, "[RAM:%s%%] ", NULL	      },
